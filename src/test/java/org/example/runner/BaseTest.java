43468524bf1b913c0d7e676adfefd6501dc26c30
EldarAbdulhakov <braid2012@gmail.com>
2025-03-31 01:58:00 +0300

    add ci/cd

--- src/test/java/org/example/runner/BaseTest.java
+++ src/test/java/org/example/runner/BaseTest.java
@@ -3,15 +3,10 @@ package org.example.runner;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.Duration;
 
 public abstract class BaseTest {
@@ -23,19 +18,8 @@ public abstract class BaseTest {
         return driver;
     }
 
-    public WebDriver remouteDriver() throws MalformedURLException {
-        URL remoteUrl = new URL("http://localhost:4444/wd/hub");
-
-        // Задаем возможности для удаленного браузера (например, Chrome)
-        ChromeOptions options = new ChromeOptions(); // или другой браузер
-
-        // Создаем удаленный WebDriver
-        return new RemoteWebDriver(remoteUrl, options);
-    }
-
     @BeforeMethod
-    public void setUp() throws MalformedURLException {
-        remouteDriver();
+    public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1920, 1080));
         getDriver().get("https://practice-automation.com/form-fields/");
